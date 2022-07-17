d2c36fc525484456c71daf674e6f2550069d22db
Alinasto <alinaoprea05@yahoo.com>
2022-07-17 14:51:54 +0300

    add shape classes

---
+++ src/Circle.java
@@ -0,0 +1,29 @@
+public class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double computeArea() {
+        double valuePI = 3.14;
+        double ACircle = valuePI * (radius * radius);
+        return ACircle;
+
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
+}
